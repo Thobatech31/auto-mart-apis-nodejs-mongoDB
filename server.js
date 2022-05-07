3464454533d22bd3eca69167513b2ca4096a0c94
Thobatech31 <owolabitoba31@gmail.com>
2022-05-07 22:08:29 +0000

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-var cors = require('cors');
+const cors = require('cors');
 
 //import Routes
 const userRoute = require('./Routes/userRoute');
