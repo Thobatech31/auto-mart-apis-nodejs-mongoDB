7932c542099c4e2e5fda2a40eb14c96aad0a8c28
Thobatech31 <owolabitoba31@yahoo.com>
2024-08-09 17:10:48 +0100

    fix

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const enrolleeRoute = require('./Routes/userRoute')
 dotenv.config();
 
 //Middlewares
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended:true, limit: '50mb'}))
